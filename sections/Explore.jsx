cac498c45a16335572e99770b19694d7e107c303
vasu <vasu@vasus-MacBook-Air.local>
2022-12-04 21:53:15 +0530

    initialized

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,14 +1,14 @@
-'use client';
+"use client";
 
-import { motion } from 'framer-motion';
-import { useState } from 'react';
-import styles from '../styles';
-import { staggerContainer } from '../utils/motion';
-import { ExploreCard, TitleText, TypingText } from '../components';
-import { exploreWorlds } from '../constants';
+import { motion } from "framer-motion";
+import { useState } from "react";
+import styles from "../styles";
+import { staggerContainer } from "../utils/motion";
+import { ExploreCard, TitleText, TypingText } from "../components";
+import { exploreWorlds } from "../constants";
 
 const Explore = () => {
-  const [active, setActive] = useState('world-2');
+  const [active, setActive] = useState("world-2");
 
   return (
     <section className={`${styles.paddings}`} id="explore">
@@ -20,7 +20,10 @@ const Explore = () => {
         className={`${styles.innerWidth} mx-auto flex flex-col`}
       >
         <TypingText title="| The World" textStyles="text-center" />
-        <TitleText title={<>Choose the world you want<br className="md:block hidden" /> to explore</>} textStyles="text-center" />
+        <TitleText
+          title={<>Choose the world you want to explore</>}
+          textStyles="text-center"
+        />
 
         <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
           {exploreWorlds.map((world, index) => (
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import { motion } from 'framer-motion';
-import styles from '../styles';
-import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { slideIn, staggerContainer, textVariant } from "../utils/motion";
 
 const Hero = () => (
   <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
@@ -14,10 +14,7 @@ const Hero = () => (
       classname={`${styles.innerWidth} mx-auto flex flex-col`}
     >
       <div className="flex justify-center items-center flex-col relative z-10">
-        <motion.h1
-          variants={textVariant(1.1)}
-          className={styles.heroHeading}
-        >
+        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
           Metaverse
         </motion.h1>
         <motion.div
@@ -31,7 +28,7 @@ const Hero = () => (
       </div>
 
       <motion.div
-        variants={slideIn('right', 'tween', 0.2, 1)}
+        variants={slideIn("right", "tween", 0.2, 1)}
         className="relative w-full md:-mt-[20px] -mt-[12px]"
       >
         <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
